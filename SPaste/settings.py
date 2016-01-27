94a0801ab7aff5181d3358995fe6c84aa07643bd
Stdio A <stdio_dai@nuaa.edu.cn>
2016-01-27 10:19:38 +0800

    Migrate to MySQL

--- SPaste/settings.py
+++ SPaste/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'SPaste.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spaste',
+        'USER': 'spaste',
+        'PASSWORD': 'daiyuchen',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
